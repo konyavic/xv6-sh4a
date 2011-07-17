48960443193714aa7fcb30b5536ed55869210fa6
Victor Lee <konya.vic@gmail.com>
2011-07-18 05:20:43 +0900

    temporary commit, reaching the c function of system call

--- exp.c
+++ exp.c
@@ -142,62 +142,38 @@ tlbprotecterrorW(){uint tlbtable[64][3];
   return;
 }
 
-addresserrorR(){;}
-
-addresserrorW(){;}
-
-fpuexception(){;}
-tlbdoulblehit(){;}
-INT_Reserved140(){;}
-
-INT_TRAPA(){;}
-
-illigalinstruction(){;}
-
-slotilligal(){;}
-
-nmi(){;}
-
-userbreak(){;}
-
-ir115(){;}
-
-ir114(){;}
-
-ir113(){;}
-
-ir112(){;}
-
-ir111(){;}
-
-ir110(){;}
-
-ir19(){;}
-
-ir18(){;}
-
-ir17(){;}
-
-ir16(){;}
-
-ir15(){;}
-
-ir14(){;}
-
-ir13(){;}
-
-ir12(){;}
-
-ir11(){;}
+void addresserrorR(){;}
+void addresserrorW(){;}
+void fpuexception(){;}
+void tlbdoulblehit(){;}
+void INT_Reserved140(){;}
+void INT_TRAPA(){;}
+void illigalinstruction(){;}
+void slotilligal(){;}
+void nmi(){;}
+void userbreak(){;}
+void ir115(){;}
+void ir114(){;}
+void ir113(){;}
+void ir112(){;}
+void ir111(){;}
+void ir110(){;}
+void ir19(){;}
+void ir18(){;}
+void ir17(){;}
+void ir16(){;}
+void ir15(){;}
+void ir14(){;}
+void ir13(){;}
+void ir12(){;}
+void ir11(){;}
 
 //ir10(){;}
 
-tuni0(){;}
-
-tuni1(){;}
-
-tuni2(){;}
-tuni3(){;}
+void tuni0(){;}
+void tuni1(){;}
+void tuni2(){;}
+void tuni3(){;}
 void tuni4(void)
 {
   tmu_mask();
@@ -222,34 +198,29 @@ void tuni4(void)
 
   return;
 }	//0x460 TUNI3
-ati(){;}		//0x480 RTC ATI
-pri(){;}		//0x4a0 PRI
-cui(){;}		//0x4c0 CUI
-eri1(){;}		//0x4e0 SCI1 ERI
-rxi1(){;}		//0x500 RXI
-txi1(){;}		//0x520 TXi
-tei1(){;}		//0x540 TEI
-iti(){;}		//0x560 WDT ITI
-rcmi(){;}		//0x580 BSC RCMI
-rovi(){;}		//0x5a0 ROVI
-
-hudi(){;}		//0x600 H-UDI
-
-dei0(){;}		//0x640 DEI0
-dei1(){;}		//0x660 DEI1
-dei2(){;}		//0x680 DEI2
-dei3(){;}		//0x6a0 DEI3
-dmae(){;}		//0x6c0 DMAE
-
-eri2(){;}		//0x700 SCIF ERI
-rxi2(){;}		//0x720 RXI
-bri2(){;}		//0x740 BRI
-txi2(){;}		//0x760 TXI
-
-fpudisable(){;}	//0x800 FPU disable
-slotfpudisable(){;}	//0x820 slot FPU disable
-
-trapa0(){;}	
+void ati(){;}		//0x480 RTC ATI
+void pri(){;}		//0x4a0 PRI
+void cui(){;}		//0x4c0 CUI
+void eri1(){;}		//0x4e0 SCI1 ERI
+void rxi1(){;}		//0x500 RXI
+void txi1(){;}		//0x520 TXi
+void tei1(){;}		//0x540 TEI
+void iti(){;}		//0x560 WDT ITI
+void rcmi(){;}		//0x580 BSC RCMI
+void rovi(){;}		//0x5a0 ROVI
+void hudi(){;}		//0x600 H-UDI
+void dei0(){;}		//0x640 DEI0
+void dei1(){;}		//0x660 DEI1
+void dei2(){;}		//0x680 DEI2
+void dei3(){;}		//0x6a0 DEI3
+void dmae(){;}		//0x6c0 DMAE
+void eri2(){;}		//0x700 SCIF ERI
+void rxi2(){;}		//0x720 RXI
+void bri2(){;}		//0x740 BRI
+void txi2(){;}		//0x760 TXI
+void fpudisable(){;}	//0x800 FPU disable
+void slotfpudisable(){;}	//0x820 slot FPU disable
+void trapa0(){;}	
 
 //                 INT_Input_Capture(){;}
 
@@ -271,5 +242,12 @@ trapa0(){;}
 
 //                 INT_REF_RCMI(){;}
 
-
-
+void system_call() {
+  uint reg;
+  cprintf("%s:\n", __func__);
+  asm volatile(
+      "stc r4_bank, %0"
+      : "=r"(reg)
+      );
+  cprintf("%s: r4_bank=\"%s\"\n", __func__, reg);
+}
